61c93d87ebde316c069b2ef2140e702d0fbb356e
sustally <tech@sustally.com>
2026-05-22 12:54:38 +0530

    remove payload css import

--- backend/src/app/(payload)/layout.tsx
+++ backend/src/app/(payload)/layout.tsx
@@ -1,29 +1,33 @@
-import type { ServerFunctionClient } from 'payload'
-import config from '@payload-config'
-import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
-import React from 'react'
+import type { ServerFunctionClient } from "payload";
+import config from "@payload-config";
+import { handleServerFunctions, RootLayout } from "@payloadcms/next/layouts";
+import React from "react";
 
-import { importMap } from './admin/importMap'
-import '@payloadcms/next/css'
-import './custom.scss'
+import { importMap } from "./admin/importMap";
+// import '@payloadcms/next/css'
+import "./custom.scss";
 
 type Args = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 const serverFunction: ServerFunctionClient = async function (args) {
-  'use server'
+  "use server";
   return handleServerFunctions({
     ...args,
     config,
     importMap,
-  })
-}
+  });
+};
 
 export default function Layout({ children }: Args) {
   return (
-    <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
+    <RootLayout
+      config={config}
+      importMap={importMap}
+      serverFunction={serverFunction}
+    >
       {children}
     </RootLayout>
-  )
+  );
 }
